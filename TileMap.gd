032359a1310fc8fb1aac034c9e8fbc3aee2dfb19
laroccrj <laroccrj@gmail.com>
2019-07-10 14:24:23 -0400

    Can move around. Camera follows player. Config.

--- TileMap.gd
+++ TileMap.gd
@@ -5,16 +5,9 @@ var pos = Vector2(0,0)
 
 func _ready():
 	_tileset = get_tileset()
+	cell_size.x = Config.grid_size
+	cell_size.y = Config.grid_size
 	spawnRoom(Vector2(0,0), 10, 10)
-
-func _input(event):
-	if event is InputEventMouseButton \
-	and event.button_index == BUTTON_LEFT \
-	and event.pressed:
-		# var tile_pos = world_to_map(event.pos)
-		var tile_pos = world_to_map(get_viewport().get_mouse_position())
-		print_debug('click')
-		set_cellv(tile_pos, _tileset.find_tile_by_name("rocky"))
 		
 func spawnRoom(start, width, height):
 	for x in range(width):
@@ -26,7 +19,4 @@ func spawnRandomBlock(pos):
 	var tile = "rocky"
 	if randi() % 2 == 0:
 		tile = "dirty"
-	print_debug('spawnRandomBlock')
-	print_debug(tile)
-	print_debug(pos)
 	set_cellv(pos, _tileset.find_tile_by_name(tile))
