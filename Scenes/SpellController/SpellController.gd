9d7f87298f735a43f64e5f8beb0747520c560fc4
laroccrj <laroccrj@gmail.com>
2019-07-11 11:54:17 -0400

    Can spawn spells with direciton

--- Scenes/SpellController/SpellController.gd
+++ Scenes/SpellController/SpellController.gd
@@ -1,13 +1,16 @@
 class_name SpellController extends Node2D
 
 var spell_count = 0
+var spell_scene = load("res://Scenes/SpellController/Spell/Spell.tscn")
 
 func _ready():
 	GlobalNodes.spell_controller = self
 	spawn_spell()
 
 func spawn_spell():
-	var spell = Spell.new()
-	spell_count += 1
-	spell.set_name('spell_' + (spell_count as String))
-	add_child(spell)
+	var spell = spell_scene.instance()
+	spell.direction = Vector2(0, 1)
+	call_deferred("add_child", spell)
+	#spell_count += 1
+	#spell.set_name('spell_' + (spell_count as String))
+	#add_child(spell)
